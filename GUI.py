baa1e7f7af5b75c98348a3026adf094f891c5b67
Ekte <Ekte@users.noreply.github.com>
2017-02-08 18:42:11 +0800

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -7,7 +7,11 @@ from tkFileDialog import askopenfilename
 from tesserocr import PyTessBaseAPI, RIL, iterate_level
 import numpy as np
 import cv2
-# import csv  # not used yet
+from skimage import feature
+from skimage import measure
+from scipy import ndimage as ndi
+import csv
+import Levenshtein  # may or may not be used
 
 class simpleapp_tk(Tkinter.Tk):
     def __init__(self, parent):  # constructor
@@ -18,7 +22,7 @@ class simpleapp_tk(Tkinter.Tk):
     def initialize(self):  # will be used for creating all the widgets
         self.width = 400
         self.height = 400
-        self.method = "Advanced"  # default
+        self.method = "Hybrid"  # default
         self.grid()  # add things here later
 
         # button for loading input images
@@ -26,7 +30,7 @@ class simpleapp_tk(Tkinter.Tk):
         button1.grid(column=0, row=1)
 
         # button for loading Advance Shipment Notice
-        button3 = Tkinter.Button(self, text=u"Load ASN (to be implemented)!", command=self.OnButtonClickASN)
+        button3 = Tkinter.Button(self, text=u"Load ASN !", command=self.OnButtonClickASN)
         button3.grid(column=2, row=1)
 
         # label for displaying raw images
@@ -55,9 +59,9 @@ class simpleapp_tk(Tkinter.Tk):
         self.label4.grid(column=0, row=2)
 
         # dropdown menu for selecting image processing procedure
-        optionList = ["Advanced", "Basic", "ContourGaussianKernelOtsu", "GaussianKernelAndOtsu", "Otsu"]  # add more later
+        optionList = ["Hybrid", "Advanced", "Basic", "ContourGaussianKernelOtsu", "GaussianKernelAndOtsu", "Otsu"]  # add more later
         self.dropVar = Tkinter.StringVar()
-        self.dropVar.set("Advanced")  # default
+        self.dropVar.set("Hybrid")  # default
         self.dropMenu1 = Tkinter.OptionMenu(self, self.dropVar, *optionList, command=self.func)
         self.dropMenu1.grid(column=1, row=1)
 
@@ -96,11 +100,17 @@ class simpleapp_tk(Tkinter.Tk):
             text, img = processor.ContourGaussianKernelOtsu()
             self.DisplayProcessed(img.resize((self.width, self.height), Image.ANTIALIAS))
             self.DisplayOCRText(text)
+        elif self.method in "Hybrid":
+            text, img = processor.Hybrid()
+            self.DisplayProcessed(img.resize((self.width, self.height), Image.ANTIALIAS))
+            self.DisplayOCRText(text)
         else:
             print "Method selection error!"
 
     def OnButtonClickASN(self):
-        pass  # implement later
+        CSVName = askopenfilename()
+        loader = CSVLoader(CSVName)
+        asn_csv_array = loader.Load()
 
     def DisplayProcessed(self, image_processed):
         image_processed = ImageTk.PhotoImage(image_processed)
@@ -120,16 +130,192 @@ class simpleapp_tk(Tkinter.Tk):
         pass  # to be implemented 
 
 class img_processor():
-    def __init__(self, filename):  
+    def __init__(self, filename):
         self.filename = filename
 
     def Benchmark(self):
         """Simple OCR with Tesseract only"""
         with PyTessBaseAPI(psm=6) as api:
+            api.SetVariable("tessedit_char_whitelist",
+                            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0987654321-.:/()")
             api.SetImageFile(self.filename)
             text_output = api.GetUTF8Text()
             image_processed = api.GetThresholdedImage()
             return text_output, image_processed
+
+    def Basic(self, thresholded):
+        with PyTessBaseAPI(psm=6) as api:
+            api.SetVariable("tessedit_char_whitelist", "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0987654321-.:/()")
+            api.SetImage(thresholded)
+            text_output = api.GetUTF8Text()
+            image_processed = api.GetThresholdedImage()
+            return text_output, image_processed
+
+    def Hybrid(self):
+        img = cv2.imread(self.filename)
+
+        # next split image into the three RGB channels
+        img_red = img[:, :, 0]
+        img_green = img[:, :, 1]
+        img_blue = img[:, :, 2]
+        print img_red.shape
+        # perform Canny edge detector on each channel since text may be found in any of the channels -- but which parameters to use?
+        edge_red = feature.canny(img_red)
+        edge_green = feature.canny(img_green)
+        edge_blue = feature.canny(img_blue)
+        edge_assimilated = np.logical_or(edge_red, np.logical_or(edge_green, edge_blue))  # boolean array
+
+        # Next, want to do both horizontal and vertical dilation with 1x3 and 3x1 structuring elements
+        # Note: paper suggests 1x3 and 3x1, but in our application 5x1 and 1x5 might work better
+        strel1 = np.zeros((5, 5))
+        for i in range(0, 5):
+            strel1[2][i] = 1
+
+        edge_dim_1 = ndi.binary_dilation(edge_assimilated, structure=strel1)
+
+        strel2 = np.zeros((5, 5))
+
+        for j in range(0, 5):
+            strel2[j][2] = 1
+
+        edge_dim_2 = ndi.binary_dilation(edge_assimilated, structure=strel2)
+
+        edges_dil_comb = np.logical_or(edge_dim_1, edge_dim_2)
+        all_labels = measure.label(edges_dil_comb, neighbors=8, connectivity=2).astype(
+            'uint8')  # same dimensions as image
+
+        im2, abac, hierarchy = cv2.findContours(all_labels.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
+
+        hierarchy = hierarchy[0]  # get the useful stuff only
+
+        # filter contours based on area (reject small ones, which are definitely noise)
+        large_contours = []
+        lower_area_bound = 700  # MUST be calibrated so that no text is removed..700 seems appropriate
+        for contour in zip(abac, hierarchy):
+            if cv2.contourArea(contour[0]) > lower_area_bound:  # contour[1][2] > 0
+                large_contours.append(contour[0])
+        print "Number of contours left: " + str(len(large_contours))  # all of these will be processed,so more -> slower
+        black_bg = np.zeros((img_red.shape[0], img_red.shape[1], 3), dtype='uint8')
+
+        # The following two lines may be commented out, they are just for visualizing the contours
+        cv2.drawContours(black_bg, large_contours, -1, (0, 255, 0), 3)
+        # Image.fromarray(black_bg).show()
+
+        # use grayscale intensities to filter: m - k*s. m is mean, s is sd (m, s con component-specific. k is parameter)
+        grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # m - k*s will work in grayscale
+
+        # find mean, SD for individual connected components
+
+        lst_intensities = []  # Initialize empty list
+
+        bg_for_final = np.zeros((img_red.shape[0], img_red.shape[1]), dtype='uint8')  # for drawing onto
+        bg_for_final += 255  # change to white background color
+
+        for i in range(len(large_contours)):  # loop through the contours
+            cimg_outline = np.zeros((img_red.shape[0], img_red.shape[1]), dtype='uint8')
+            cv2.drawContours(cimg_outline, large_contours, i, color=255, thickness=1)  # Draw the contour outline only
+            pts_outline = np.where(cimg_outline == 255)
+            gs_vals_outline = grayscale[pts_outline[0], pts_outline[1]]
+            intensity_fg = np.mean(gs_vals_outline)
+
+            cimg_inside = np.zeros((img_red.shape[0], img_red.shape[1]), dtype='uint8')
+            cv2.drawContours(cimg_inside, large_contours, i, color=255,
+                             thickness=-1)  # Thickness -1 will fill in the entire contour. i is for drawing individual contours
+
+            # Stats for the entire connected component:
+            pts_cp = np.where(cimg_inside == 255)
+            mean_cp, sd_cp = cv2.meanStdDev(grayscale[pts_cp[0], pts_cp[1]])
+
+            # Stats for inside (excludes boundary pts) -- may need to exclude more pts later!! (thickness)
+            cimg_inside_only = cimg_inside - cimg_outline  # subtract the boundaries from the contours
+            pts_inside = np.where(cimg_inside_only == 255)
+            gs_vals_inside = grayscale[pts_inside[0], pts_inside[1]]
+            intensity_bg = np.mean(gs_vals_inside)
+
+            # Thresholding (want to cvt to binary, and remove non-text pixels)
+            if intensity_fg < intensity_bg:  # Note: this part is changed from the paper (inverted, actually)
+                k_cp = 0.05
+                threshold_cp = mean_cp - (k_cp * sd_cp)
+                ret, thresh = cv2.threshold(grayscale.copy(), threshold_cp, 255,
+                                            cv2.THRESH_BINARY)  # originally THRESH_BINARY_INV
+                mask_accepted = cv2.bitwise_and(thresh, cimg_inside)
+            else:
+                k_cp = 0.40
+                threshold_cp = mean_cp - (k_cp * sd_cp)
+                ret, thresh = cv2.threshold(grayscale.copy(), threshold_cp, 255,
+                                            cv2.THRESH_BINARY_INV)  # originally THRESH_BINARY
+                mask_accepted = cv2.bitwise_and(thresh, cimg_inside)
+
+            bg_for_final -= mask_accepted
+
+        bg_for_final = 255 - bg_for_final
+        # Image.fromarray(bg_for_final).show()
+        # cv2.imwrite('outputcool.png', img & cv2.cvtColor(cimg, cv2.COLOR_GRAY2BGR))
+
+        # Find the contours (in binary)
+        im2_out, conts, hierch = cv2.findContours(bg_for_final, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+
+        accepted_conts = []
+        largest_area = 2000  # this is just for debugging, to be removed later
+        for i in range(0, len(conts)):
+            area = cv2.contourArea(conts[i])
+            if area > 700:
+                accepted_conts.append(conts[i])
+                if area > largest_area:  # for debugging
+                    largest_area = area
+
+        print "Largest contour area: " + str(largest_area)
+
+        bg_for_density_prime = np.zeros((img_red.shape[0], img_red.shape[1]), dtype='uint8')
+
+        for i in range(0, len(accepted_conts)):
+            bg_for_density = np.zeros((img_red.shape[0], img_red.shape[1]), dtype='uint8')
+            cv2.drawContours(bg_for_density, accepted_conts, i, color=255, thickness=-1)
+            pts_col = np.where(bg_for_density == 255)
+            cv2.drawContours(bg_for_density_prime, accepted_conts, i, color=255, thickness=-1)
+            # Get values, both zero and non-zero
+            vals_for_numerator = bg_for_final[pts_col[0], pts_col[1]]
+            vals_for_denominator = bg_for_density[pts_col[0], pts_col[1]]
+
+            numerator = 0
+
+            for ele in vals_for_numerator:
+                if ele > 100:
+                    numerator += 1
+
+            denominator = len(vals_for_denominator)
+            # print denominator
+
+            density = 1.0 * numerator / denominator
+            # print " Density: " + str(density)
+            threshold_density = 0.20  # calibration needed
+            # The following part does pretty much nothing so far
+            if density > threshold_density:  # this rarely triggers, look into how to apply it
+                bg_for_final[pts_col[0], pts_col[1]] = 0
+                print " Triggered, m8! "
+
+        # Do comparison: logical_and for the two background images (nonzero)
+
+        final_result_I_hopeD = np.logical_and(bg_for_final, bg_for_density_prime)
+
+        keep_going = final_result_I_hopeD.astype('uint8') * 255  # values 0 or 255 only
+
+        # Image.fromarray(keep_going).show()
+
+        where_to_look = np.where(keep_going == 255)
+        gs_mean, gs_sd = cv2.meanStdDev(grayscale[where_to_look[0], where_to_look[1]])
+        thresh_gs_val = gs_mean + (0.25 * gs_sd)
+        ret, thresh = cv2.threshold(grayscale.copy(), thresh_gs_val, 255,
+                                    cv2.THRESH_BINARY)  # this is a global threshold with carefully derived threshold
+        # Image.fromarray(thresh).show()  # is entire image, want overlapping parts only
+        the_end_mate = np.logical_and(255 - thresh, keep_going)  # combine the results from global and local thresholds
+        the_end_image = Image.fromarray(the_end_mate.astype('uint8') * 255)
+        # the_end_image.show()
+        # cv2.imwrite('outputbinary.png', 255 - the_end_mate.astype('uint8') * 255)
+
+        img_ret, text_ret = self.Basic(the_end_image)
+
+        return img_ret, text_ret
 
     def Advanced(self):
         """Does contour analysis to find the likely text region(s) of an image,
@@ -139,7 +325,7 @@ class img_processor():
         img_raw = cv2.imread(self.filename)
 
         # convert to grayscale
-        img_grey = cv2.cvtColor(img_raw, cv2.COLOR_BGR2GRAY)
+        img_grey = cv2.cvtColor(img_raw, cv2.COLOR_BGR2GRAY)  # compare BGR and RGB -- results appear to be the same
         binary = img_grey.copy()  # change name later
 
         # convert to float32
@@ -449,10 +635,122 @@ class img_processor():
         return output  # Boolean
 
 class CSVLoader():
-    pass  # implementation subject to format of Unloading Report
+    """Loads the ASN file from csv format, and stores all the values in an array"""
+    def __init__(self, ASNfile):
+        self.ASNfile = ASNfile
+
+    def Load(self):
+        """Load and return the array"""
+        asn_data = []
+
+        with open(self.ASNfile, "rb") as csvfile:  # load the ASN from csv file, store in array (line by line)
+            spamreader = csv.reader(csvfile, delimiter=';',
+                                    quotechar='|')  # Spamreader is an object..need to save the info somehow
+
+            for row in spamreader:  # Row only contains 1 row at any given time
+                print ', '.join(row)
+                asn_data.append(row)
+        csvfile.close()
+
+        return asn_data
 
 class outputInterpreter():
-    pass  # implement later 
+    def __init__(self, OCR_output):
+        self.OCR_output = OCR_output
+        # may or may not need to use string.encode('utf-8')
+
+    def subString6Digit(self, input_string):
+        """Check if have at least 6 digits in a row, return true if yes"""
+        digits = False
+        for x in range(6, len(input_string)):
+            if input_string[x-6:x].isdigit():
+                digits = True
+                break
+        return digits
+
+    def isItemCode(self, text_line):
+        """Should be either the first or second line normally. Consist of mainly digits"""
+        # find the general form of the text_line, see if any substring matches the format of item code
+        # translate the string, and check if have a long substring of digits, which will indicate it's an item code.
+        input_string = text_line.strip('\n')  # get rid of the end-line character
+        input_string = input_string.strip(" ")
+        input_string = input_string.upper()
+        input_string = input_string.replace("O", "0")
+        input_string = input_string.replace("L", "1")
+        input_string = input_string.translate(None, '-_() ')
+
+        if input_string.isdigit():
+            return True
+        elif self.subString6Digit(input_string):
+            return True
+        else:
+            return False
+
+    def isSolidCode(self, input_string):
+        """Digits mainly, lots of zeros. If handwritten, then will be short and nonsense
+        Check if have triple 0s, allowing o and O. Consider allowing 6"""
+        input_string = input_string.strip('\n')
+        input_string = input_string.strip(" ")
+        input_string = input_string.upper()
+        input_string = input_string.replace("O", "0")
+        input_string = input_string.translate(None, '-_() ')
+
+        if "00" in input_string:  # consider adding more cases
+            return True
+        else:
+            return False
+
+    def isAssortCode(self):
+        """One char followed by 3 digits"""
+        pass
+
+    def isDescription(self):
+        """Lots of characters"""
+        pass
+
+    def isQuantity(self, input_string):
+        """Contains hints like QTY, PCS"""
+        pass
+
+    def isCartonNo(self):
+        """Has some indicators..."""
+        pass
+
+    def isNothing(self):
+        pass
+
+    def categorizeLines(self):
+        # Want to store the line indexes of each type of line
+        categories = {'Itemcode': False, 'Solidcode': False, "Assortcode": False, 'Quantity': False, 'Cartonnumber': False, 'Description': False, 'Nothing': False}
+
+        # split the string around "\n", and store the result in an array
+        text_array = self.OCR_output.split(sep="\n")  # want to use the newline character as separator
+        while not categories['Itemcode']:  # Need to make sure the loop stops once Itemcode becomes a digit
+            # what if there is no line categorized as item code????
+            for i in range(0, len(text_array)): # will be a long string rather than an array of string (separated by newline character)
+                if self.isItemCode(text_array[i]):  # don't need to check after have found the line corresponding to item code (save comp time)
+                    # can use while loop
+                    categories['Itemcode'] = i  # store the index of the item code
+                    break
+                if i == len(categories) and not ['Itemcode']:
+                    print "Unable to find Itemcode, we have a problem m8!"
+                    break
+        # now only consider lines with index > i (assume no sensible output is above the item code
+        # May have error if i == len(text_array), but in this case we're screwed anyways
+        for j in range(i+1, len(text_array)-1):  # next LF solid/assort code and try to categorize
+            """if j+1 contains "PCS", then most likely line j is our solid/assort code, regardless of the ugliness of line j's text
+            solid code is more common than assort code, so first LF solid"""
+            if self.isSolidCode(text_array[j]):
+                categories['Solidcode'] = j
+                break
+        # consider maximum allowable number of lines checked: should not have too much noise
+        # assume absense of solid code means we have assort code?
+        # but: in some cases messy text comes out
+
+
+class matcher():
+    def __init__(self, csv_array):
+        self.csv_array = csv_array
 
 if __name__ == "__main__":
     app = simpleapp_tk(None)  # No parent because first element
